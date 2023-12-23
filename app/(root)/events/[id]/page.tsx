4347ad8bfcf0c493c6fc5ba7d166f0c970d86eea
ignatiygotovchikov <ignatsergeevich10@gmail.com>
2023-12-23 14:53:36 +0200

    app finished...

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,13 +1,12 @@
+import CheckoutButton from "@/components/shared/CheckoutButton";
+import Collection from "@/components/shared/Collection";
 import {
   getEventById,
   getRelatedEventsByCategory,
 } from "@/lib/actions/event.actions";
-import { SearchParamProps } from "@/types";
-import React from "react";
-import Image from "next/image";
 import { formatDateTime } from "@/lib/utils";
-import Collection from "@/components/shared/Collection";
-import CheckoutButton from "@/components/shared/CheckoutButton";
+import { SearchParamProps } from "@/types";
+import Image from "next/image";
 
 const EventDetails = async ({
   params: { id },
@@ -24,12 +23,12 @@ const EventDetails = async ({
   return (
     <>
       <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
-        <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7lx">
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
           <Image
             src={event.imageUrl}
             alt="hero image"
-            height={1000}
             width={1000}
+            height={1000}
             className="h-full min-h-[300px] object-cover object-center"
           />
 
@@ -42,12 +41,12 @@ const EventDetails = async ({
                   <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">
                     {event.isFree ? "FREE" : `$${event.price}`}
                   </p>
-                  <p className="p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5  text-grey-500">
+                  <p className="p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-grey-500">
                     {event.category.name}
                   </p>
                 </div>
 
-                <p className="p-medium-18 ml-2 mt-2 sm:mt-1">
+                <p className="p-medium-18 ml-2 mt-2 sm:mt-0">
                   by{" "}
                   <span className="text-primary-500">
                     {event.organizer.firstName} {event.organizer.lastName}
@@ -85,12 +84,12 @@ const EventDetails = async ({
                   width={32}
                   height={32}
                 />
-                <p className="p-medium-16 lg:p-medium-20">{event.location}</p>
+                <p className="p-medium-16 lg:p-regular-20">{event.location}</p>
               </div>
             </div>
 
             <div className="flex flex-col gap-2">
-              <p className="p-bold-20 text-grey-600">What y`ll learn!</p>
+              <p className="p-bold-20 text-grey-600">What You'll Learn:</p>
               <p className="p-medium-16 lg:p-regular-18">{event.description}</p>
               <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 underline">
                 {event.url}
@@ -100,16 +99,17 @@ const EventDetails = async ({
         </div>
       </section>
 
-      {/* Events with the same category */}
+      {/* EVENTS with the same category */}
       <section className="wrapper my-8 flex flex-col gap-8 md:gap-12">
         <h2 className="h2-bold">Related Events</h2>
+
         <Collection
           data={relatedEvents?.data}
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
           collectionType="All_Events"
-          limit={6}
-          page={1}
+          limit={3}
+          page={searchParams.page as string}
           totalPages={relatedEvents?.totalPages}
         />
       </section>
--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,7 +1,7 @@
 import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
 import { getEventsByUser } from "@/lib/actions/event.actions";
-// import { getOrdersByUser } from '@/lib/actions/order.actions'
+import { getOrdersByUser } from "@/lib/actions/order.actions";
 import { IOrder } from "@/lib/database/models/order.model";
 import { SearchParamProps } from "@/types";
 import { auth } from "@clerk/nextjs";
@@ -15,9 +15,9 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const ordersPage = Number(searchParams?.ordersPage) || 1;
   const eventsPage = Number(searchParams?.eventsPage) || 1;
 
-  // const orders = await getOrdersByUser({ userId, page: ordersPage})
+  const orders = await getOrdersByUser({ userId, page: ordersPage });
 
-  // const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
   const organizedEvents = await getEventsByUser({ userId, page: eventsPage });
 
   return (
@@ -34,14 +34,14 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
 
       <section className="wrapper my-8">
         <Collection
-          data={organizedEvents?.data}
+          data={orderedEvents}
           emptyTitle="No event tickets purchased yet"
           emptyStateSubtext="No worries - plenty of exciting events to explore!"
           collectionType="My_Tickets"
           limit={3}
           page={ordersPage}
           urlParamName="ordersPage"
-          totalPages={organizedEvents?.totalPages}
+          totalPages={orderedEvents?.totalPages}
         />
       </section>
 
